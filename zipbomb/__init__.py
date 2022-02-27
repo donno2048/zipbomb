17219cec1b741c7bba789a2b71603cb7d80aa69e
Elisha Hollander <just4now666666@gmail.com>
2022-02-27 14:25:44 +0200

    Update __init__.py

--- zipbomb/__init__.py
+++ zipbomb/__init__.py
@@ -6,7 +6,6 @@ def D(m,v,A=0):
 	return A
 def I(K):
 	C,D,G=K.values(),{0:0},{};B={A:C.count(A)for A in C}
-	for A in C:B[A]=B.get(A,0)+1
 	for A in range(max(C)):D[A+1]=D[A]+B.get(A,0)<<1
 	for(H,A)in sorted(K.items(),key=lambda x:(x[1],x[0])):G[H]=D[A],A;D[A]+=1
 	return G
